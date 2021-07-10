c637760dc0fbc883233d6216f0016e4c074f62ad
zl3ysh1y <87120212+zl3ysh1y@users.noreply.github.com>
2021-07-10 14:11:09 +0600

    Delete HW2.py

--- HW2.py
+++
@@ -1,6 +0,0 @@
-time = int(input("Введите время в секундах: "))
-h = time // 3600
-min = (time - h * 3600) // 60
-sec = time - (h * 3600 + min * 60)
-print(f"Время в формате чч:мм:сс    {h}:{min}:{sec}")
-
